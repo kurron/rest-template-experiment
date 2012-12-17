0ff634d263aee67e9a37e439d0fcaae93a8b047c
Ronald D. Kurr <kurr@kurron.org>
2012-12-17 15:24:02 -0500

    Got it to launch.

---
+++ src/main/java/org/kurron/rest/org/kurron/rest/RestTemplateExercise.java
@@ -0,0 +1,12 @@
+package org.kurron.rest.org.kurron.rest;
+
+/**
+ * A simple learning test to see how Spring's RestTemplate deals with large HTTP uploads.
+ */
+public class RestTemplateExercise {
+
+    public static void main( final String[] args ) throws Exception
+    {
+        System.err.println("Hello!");
+    }
+}
